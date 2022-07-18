4c36ebb78fc9b3b54e710c508e8d2dbd7dc0d6f8
Saha202045 <albathsaada3@gmail.com>
2022-07-18 14:25:15 +0100

    Creation des attributs, getters et setters de la classe Produit et mise a la valeur nulle de la quantite en stock ; description par defaut

--- magasin.php
+++ magasin.php
@@ -4,6 +4,48 @@ class Magasin{
 }
 
 class Produit{
+    private string $nom;
+    private int $prixAchat;
+    private int $prixVente;
+    private int $nbExemplaire;
+    private string $description;
+    public function getNom(){
+        return $this->nom;
+    }
+    public function getPrixAchat(){
+        return $this->prixAchat;
+    }
+    public function getPrixVente(){
+        return $this->prixVente;
+    }
+    public function getNbExemplaire(){
+        return $this->nbExemplaire;
+    }
+    public function getDescription(){
+        return $this->description;
+    }
+    public function setNom(string $nom):string{
+        $this->nom=$nom;
+    }
+    public function setPrixAchat(int $prixAchat):int{
+        $this->prixAchat=$prixAchat;
+    }
+    public function setPrixVente(int $prixVente):int{
+        $this->prixAchat=$prixVente;
+    }
+    public function setNbExemplaire(int $nbExemplaire):int{
+        $this->nbExemplaire=$nbExemplaire;
+    }
+    public function setDescription(string $description):string{
+        $this->description=$description;
+    }
+    public function __construct(string $nom, int $prixAchat, int $prixVente){
+        $this->nom=$nom;
+        $this->prixAchat=$prixAchat;
+        $this->prixVente=$prixVente;
+    }  
+    $description="pas defaut";
+    $nbExemplaire=0;
 
 }
 class Livre extends Produit{
